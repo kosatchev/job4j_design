ae4067867b96dc76d36c283e0bebd38375232130
kosatchev <kosatchev@yandex.com>
2020-06-23 21:58:39 +0500

    5.2.2. Реализовать Store<T extends Base>[157#289388]

--- chapter_001/src/main/java/ru/job4j/generic/MemStore.java
+++ chapter_001/src/main/java/ru/job4j/generic/MemStore.java
@@ -6,7 +6,17 @@ import java.util.List;
 public final class MemStore<T extends Base> implements Store<T> {
 
 	private final List<T> mem = new ArrayList<>();
-		
+	
+	private int indexOf(String id) {
+		int rsl = -1;
+		for (int i = 0; i < this.mem.size(); i ++) {
+			if (this.mem.get(i).getId().equals(id)) {
+				rsl = i;
+			}
+		}
+		return rsl;
+	}
+	
 	@Override
 	public void add(T model) {
 		this.mem.add(model);
@@ -14,24 +24,12 @@ public final class MemStore<T extends Base> implements Store<T> {
 
 	@Override
 	public boolean replace(String id, T model) {
-		boolean rsl = false;
-		for (int i = 0; i < this.mem.size(); i ++) {
-			if (this.mem.get(i).getId().equals(id)) {
-				rsl = model == this.mem.set(i, model);
-			}
-		}
-		return rsl;
+		return model == this.mem.set(this.indexOf(id), model);
 	}
 
 	@Override
 	public boolean delete(String id) {
-		boolean rsl = false;
-		for (int i = 0; i < this.mem.size(); i ++) {
-			if (this.mem.get(i).getId().equals(id)) {
-				rsl = this.mem.remove(this.mem.get(i));
-			}
-		}
-		return rsl;
+		return this.mem.get(this.indexOf(id)) == this.mem.remove(this.indexOf(id));
 	}
 
 	@Override
