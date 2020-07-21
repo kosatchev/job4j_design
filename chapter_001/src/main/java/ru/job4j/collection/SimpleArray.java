861832798cae33bcadfd4600556a8dc9e2fb3c6e
kosatchev <kosatchev@yandex.com>
2020-07-22 01:15:38 +0500

    1. Динамический список на массиве.[158#289371]

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,67 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+
+	private int size = 0;
+	private int position = 0;
+	private int modCount = 0;
+	private Object[] array;
+
+	public SimpleArray() {
+		array = new Object[10];
+	}
+
+	public SimpleArray(int size) {
+		this.size = size;
+		this.array = new Object[size];
+	}
+
+	public T get(int index) {
+		Objects.checkIndex(index, this.position);
+		return (T) array[index];
+	}
+
+	public void add(T model) {
+		if (this.position == this.size) {
+			this.size = array.length + (this.array.length / 2) + 1;
+			this.array = Arrays.copyOf(this.array, this.size);
+		}
+		this.array[position++] = model;
+		this.modCount++;
+	}
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private final int expectedModCount = modCount;
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                checkModCount();
+                return this.index < position;
+            }
+
+            @Override
+            public T next() {
+                checkModCount();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) array[index++];
+            }
+
+            private void checkModCount() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+    }
+}
--- chapter_001/src/test/java/ru/job4j/collection/SimpleArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/SimpleArrayTest.java
@@ -1,58 +1,85 @@
 package ru.job4j.collection;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Test;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class SimpleArrayTest {
 
-	@Test
-	public void whenAddThenGet() {
-		SimpleArray<String> array = new SimpleArray<>();
-		array.add("first");
-		String rsl = array.get(0);
-		assertThat(rsl, is("first"));
-	}
-
-	@Test
-	public void whenAddThenIt() {
-		SimpleArray<String> array = new SimpleArray<>();
-		array.add("first");
-		String rsl = array.iterator().next();
-		assertThat(rsl, is("first"));
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void whenGetEmpty() {
-		SimpleArray<String> array = new SimpleArray<>();
-		array.get(0);
-	}
-
-	@Test(expected = IndexOutOfBoundsException.class)
-	public void whenGetOutBound() {
-		SimpleArray<String> array = new SimpleArray<>();
-		array.add("first");
-		array.get(1);
-	}
-
-	@Test(expected = NoSuchElementException.class)
-	public void whenGetEmptyFromIt() {
-		SimpleArray<String> array = new SimpleArray<>();
-		array.iterator().next();
-	}
-
-	@Test(expected = ConcurrentModificationException.class)
-	public void whenCorruptedIt() {
-		SimpleArray<String> array = new S, impleArray
-		<>();
+    @Test
+    public void whenAddThenGet() {
+        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
-		Iterator<String> it = array.iterator();
-		array.add("second");
-		it.next();
-	}
+        String rsl = array.get(0);
+        assertThat(rsl, is("first"));
+    }
+
+    @Test
+    public void whenAddElevenElements() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("1");
+        array.add("2");
+        array.add("3");
+        array.add("4");
+        array.add("5");
+        array.add("6");
+        array.add("7");
+        array.add("8");
+        array.add("9");
+        array.add("10");
+        array.add("11");
+        String rsl = array.get(10);
+        assertThat(rsl, is("11"));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("1");
+        String rsl = array.iterator().next();
+        assertThat(rsl, is("1"));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetEmpty() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetOutBound() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("1");
+        array.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenAddOneAndGetTwoFromIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("1");
+        Iterator<String> it = array.iterator();
+        it.next();
+        it.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("1");
+        Iterator<String> it = array.iterator();
+        array.add("2");
+        it.next();
+    }
+
 }
