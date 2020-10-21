eb82440ebdae317645a7d43871d940849d6a49e4
kosatchev <kosatchev@yandex.com>
2020-10-22 02:08:37 +0500

    2. Добавить метод boolean isBinary()[1712#289406]

--- chapter_001/src/main/java/ru/job4j/collection/Tree.java
+++ chapter_001/src/main/java/ru/job4j/collection/Tree.java
@@ -3,38 +3,55 @@ package ru.job4j.collection;
 import java.util.*;
 
 class Tree<E> implements SimpleTree<E> {
-    private final Node<E> root;
 
-    Tree(final E root) {
-        this.root = new Node<>(root);
-    }
-	
-    @Override
-    public boolean add(E parent, E child) {
-        if (findBy(child).isPresent()) {
-            return false;
-        } else {
-            return findBy(parent).get().children.add(new Node<>(child));
-        }
-    }
-	
-	    private boolean addNode(Node<E> parentNode, E child) {
-        return parentNode.children.add(new Node<>(child));
-    }
+	private final Node<E> root;
 
-    @Override
-    public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.children);
-        }
-        return rsl;
-    }
+	Tree(final E root) {
+		this.root = new Node<>(root);
+	}
+
+	@Override
+	public boolean add(E parent, E child) {
+		if (findBy(child).isPresent()) {
+			return false;
+		} else {
+			return findBy(parent).get().children.add(new Node<>(child));
+		}
+	}
+
+	private boolean addNode(Node<E> parentNode, E child) {
+		return parentNode.children.add(new Node<>(child));
+	}
+
+	public boolean isBinary() {
+		if (this.root.children.size() > 2) {
+			return false;
+		}
+		Queue<Node<E>> data = new LinkedList<>();
+		data.offer(this.root);
+		while (!data.isEmpty()) {
+			Node<E> el = data.poll();
+			if (el.children.size() > 2) {
+				return false;
+			}
+			data.addAll(el.children);
+		}
+		return true;
+	}
+
+	@Override
+	public Optional<Node<E>> findBy(E value) {
+		Optional<Node<E>> rsl = Optional.empty();
+		Queue<Node<E>> data = new LinkedList<>();
+		data.offer(this.root);
+		while (!data.isEmpty()) {
+			Node<E> el = data.poll();
+			if (el.value.equals(value)) {
+				rsl = Optional.of(el);
+				break;
+			}
+			data.addAll(el.children);
+		}
+		return rsl;
+	}
 }
