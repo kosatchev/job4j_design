7ac0d07016919e7c58e292c8191b0af14332c5a0
kosatchev <kosatchev@yandex.com>
2020-11-09 22:42:18 +0500

    3.0. Тестирование IO[173905#289416]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,23 +1,40 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 public class AnalizyTest {
 
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+
 	@Test
-	public void whenPairWithoutComment() {
-		String source = "server.log.txt";
-		String target = "unavailable.csv";
+	public void whenPairWithoutComment() throws IOException {
+		File source = folder.newFile("server.log.txt");
+		File target = folder.newFile("unavailable.csv");
+
+		try (PrintWriter out = new PrintWriter(source)) {
+			out.println("200 10:56:01\n"
+					+ "200 10:57:01\n"
+					+ "400 10:58:01\n"
+					+ "200 10:59:01\n"
+					+ "500 11:01:02\n"
+					+ "200 11:02:02");
+		}
 
 		Analizy an = new Analizy();
-		an.unavailable(source, target);
+
+		an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
 		try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
 			assertThat(reader.readLine(), is("10:58:01;10:59:01"));
 			assertThat(reader.readLine(), is("11:01:02;11:02:02"));
