17b363521a1413acfdf8215b93cf6037b759090e
kosatchev <kosatchev@yandex.com>
2020-11-10 15:02:43 +0500

    4.0. File[252491#289421]

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+
+	public static void main(String[] args) {
+
+		File file;
+		if (System.getProperty("os.name").equals("Windows")) {
+			file = new File("c:\\projects");
+		} else {
+			file = new File("../");
+		}
+
+		if (!file.exists()) {
+			throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+		}
+		if (!file.isDirectory()) {
+			throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+		}
+		System.out.println(String.format("size : %s", file.getTotalSpace()));
+		for (File subfile : file.listFiles()) {
+			System.out.printf("%s %d bytes %n", subfile.getName(), subfile.length());
+		}
+	}
+}
