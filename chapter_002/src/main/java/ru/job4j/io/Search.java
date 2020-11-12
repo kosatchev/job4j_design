b335ae40fd32e3921216842ced3bb4c5160ece56
kosatchev <kosatchev@yandex.com>
2020-11-13 02:32:36 +0500

    5. Валидация параметров запуска.[246865#289417] - Добавлена обработка ошибок

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -2,17 +2,30 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Search {
-	public static void main(String[] args) throws IOException {
-		Path path = Paths.get(args[0]);
-		search(path, args[1]).forEach(System.out::println);
+
+	public static void main(String[] args) {
+		try {
+			Path path = Paths.get(args[0]);
+			search(path, args[1]).forEach(System.out::println);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Ошибка. Необходимо запускать с 2 аргументами (путь к директории и искомое расширение)");
+		}
 	}
 
-	public static List<Path> search(Path root, String ext) throws IOException {
-		SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
-		Files.walkFileTree(root, searcher);
-		return searcher.getSearchPaths();
+	public static List<Path> search(Path root, String ext) {
+		List<Path> rsl = new ArrayList<>();
+		try {
+			SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+			Files.walkFileTree(root, searcher);
+			rsl = searcher.getSearchPaths();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		return rsl;
+
 	}
 }
