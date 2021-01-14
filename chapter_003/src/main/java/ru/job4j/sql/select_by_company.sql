9021370520f1654ad74d6e241ea3003b815d0f61
kosatchev <kosatchev@yandex.com>
2021-01-15 04:43:14 +0500

    SQL Select by company [#289361]

---
+++ chapter_003/src/main/java/ru/job4j/sql/select_by_company.sql
@@ -0,0 +1,38 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer,
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+-- 1. В одном запросе получить
+-- имена всех person, которые не состоят в компании с id = 5;
+-- название компании для каждого человека.
+select p."name", c."name" 
+from person p
+join company c on c.id = p.company_id
+where company_id = 5;
+
+-- 2. Необходимо выбрать название компании с максимальным количеством человек + количество человек в этой компании.
+select s."name", count from(
+	select c."name", count(p.company_id)
+	from company c
+	join person p on p.company_id = c.id
+	group by c."name", p.company_id
+) as s
+where s.count = (
+	select count(p.company_id)
+	from company c
+	join person p on p.company_id = c.id
+	group by p.company_id
+	order by count desc limit 1
+)
+group by s.name, s.count;
